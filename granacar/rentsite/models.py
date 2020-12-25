1279f6dee2e3116b64443e33276d2dd8e344756f
maxisuarez <maxisuarez@corrreo.ugr.es>
2020-12-26 00:11:36 +0100

    new models

--- granacar/rentsite/models.py
+++ granacar/rentsite/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Puesto(models.Model):
+  nombre_puesto = models.CharField(max_length=40, primary_key=True)
+  departamento = models.CharField(max_length=40)
+  numero_de_vacantes = models.IntegerField()
+  aptitudes_necesarias = models.CharField(max_length=250, blank = True, null = True)
+  requisitos_puesto = models.CharField(max_length=250, blank = True, null = True)
+  sueldo = models.IntegerField()
+
 class Factura(models.Model):
     #Hay que fijar en el formulario que la longitud mínima sea tmb 5, el id siempre tiene longitud 5
     id=models.CharField(max_length=5, primary_key=True)
@@ -68,13 +76,52 @@ class Vehiculo(models.Model):
 
 
 class Producto(models.Model):
-    factura = models.ForeignKey(Factura,on_delete=models.CASCADE)
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
     idProducto = models.IntegerField()
     nombre = models.CharField(max_length=10)
     cantidad = models.IntegerField()
     precio = models.IntegerField()
 
+
+class EmpleadoTrabaja(models.Model):
+  idEmpleado = models.CharField(max_length=10, primary_key=True)
+  nombre = models.CharField(max_length=20)
+  apellidos = models.CharField(max_length=20)
+  nombre_puesto = models.ForeignKey(Puesto, on_delete=models.CASCADE)
+  cuenta_bancaria = models.CharField(max_length=24)
+  fecha_pago = models.DateField()
+  fecha_alta = models.DateField()
+  fecha_baja = models.DateField(blank=True, null=True)
+
+
+class BalanceFinanciero(models.Model):
+  idBalance = models.CharField(max_length=10)
+  fecha_realizacion = models.DateField()
+  total_gastos = models.DecimalField(max_digits=20,decimal_places=2)
+  total_ingresos = models.DecimalField(max_digits=20,decimal_places=2)
+  bienes_actuales = models.DecimalField(max_digits=20,decimal_places=2)
+  deudas = models.DecimalField(max_digits=20,decimal_places=2)
+
+
+
+
+
 """
+class ConsultaEmpleado(models.Model):
+  class Meta:
+        unique_together = (('balance', 'empleado'),)
+  balance =  models.ForeignKey(BalanceFinanciero, on_delete=models.CASCADE)
+  empleado = models.ForeignKey(EmpleadoTrabaja, on_delete=models.CASCADE)
+
+
+class ConsultaInformeContable(models.Model):
+    informe=models.ForeignKey(InformeContable,primary_key=True,on_delete=models.CASCADE)
+    balance=models.ForeignKey(BalanceFinanciero,on_delete=models.CASCADE)   
+        class Meta:
+	        verbose_name_plural ="consultas informes contables"
+            unique_together = (('informe', 'balance'),)
+
+
 class Contiene(models.Model):
     alquiler = models.ForeignKey(SolicitaAlquiler, primary_key=True)
     vehiculo = models.ForeignKey(Vehiculo)
@@ -83,3 +130,4 @@ class ConsultaFactura(models.Model):
     balanceFinaciero = models.ForeignKey(BalanceFinanciero, primary_key=True)
     factura = models.ForeignKey(Factura)
 """
+
