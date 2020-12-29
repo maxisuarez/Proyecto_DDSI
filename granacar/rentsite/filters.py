b20805193bcc0bcd82f8c075b13ea647c28bad73
Pilar <pilarnavar@hotmail.es>
2020-12-29 23:29:04 +0100

    Add some new filters

--- granacar/rentsite/filters.py
+++ granacar/rentsite/filters.py
@@ -1,31 +1,60 @@
 import django_filters
+from django_filters import DateFilter, CharFilter
 
 from .models import *
 
 class PuestoFilter(django_filters.FilterSet):
     class Meta:
         model = Puesto
-        fields = '__all__'
+        fields = { 
+            'departamento':['exact'],
+            'nombre_puesto':['icontains'],
+            'numero_de_vacantes':['gte','lte'],
+            'sueldo':['gte','lte'],
+            'aptitudes_necesarias':['icontains'],
+            'requisitos_puesto':['icontains'],
+        }
 
 class InformeFilter(django_filters.FilterSet):
     class Meta:
         model = InformeContable
-        fields = '__all__'
+        fields =  { 
+            'id':['exact'],
+            'fecha': ['gte','lte'],
+        }
 
 class BienFilter(django_filters.FilterSet):
     class Meta:
         model = Bien
-        fields = '__all__'
+        fields = { 
+            'id':['exact'],
+            'valor': ['gte','lte'],
+            'nombre':['icontains'],
+            'descripcion':['icontains'],
+            'informe':['exact'],
+        }
 
 class VehiculoFilter(django_filters.FilterSet):
     class Meta:
         model = Vehiculo
-        fields = '__all__'
+        fields = { 
+            'matricula':['exact'],
+            'numero_pasajeros': ['gte','lte'],
+            'combustible':['icontains'],
+            'trasmision':['icontains'],
+            'tipo':['icontains'],
+        }
 
 class ProductoFilter(django_filters.FilterSet):
     class Meta:
         model = Producto
-        fields = '__all__'
+        fields = { 
+            'idProducto':['exact'],
+            'cantidad': ['gte','lte'],
+            'nombre':['icontains'],
+            'precio': ['gte','lte'],
+            'factura':['exact'],
+        }
 
 class ContieneFilter(django_filters.FilterSet):
     class Meta:
@@ -41,12 +70,21 @@ class ConsultaEmpleadoFilter(django_filters.FilterSet):
 class ClienteFilter(django_filters.FilterSet):
     class Meta:
         model = Cliente
-        fields = '__all__'
+        fields = { 
+            'dni':['exact'],
+            'nombrecliente':['icontains'],
+        }
 
 class SolicitaAlquilerFilter(django_filters.FilterSet):
     class Meta:
         model = SolicitaAlquiler
-        fields = '__all__'
+        fields = { 
+            'idAlquiler':['exact'],
+            'ganancia': ['gte','lte'],
+            'duracion': ['gte','lte'],
+            'precio': ['gte','lte'],
+            'cliente':['exact'],
+        }
 
 class ConsultaAlquilerFilter(django_filters.FilterSet):
     class Meta:
@@ -56,7 +94,15 @@ class ConsultaAlquilerFilter(django_filters.FilterSet):
 class BalanceFinancieroFilter(django_filters.FilterSet):
     class Meta:
         model = BalanceFinanciero
-        fields = '__all__'
+        fields = { 
+            'idBalance':['exact'],
+            'fecha_realizacion': ['gte','lte'],
+            'total_gastos':['gte','lte'],
+            'total_ingresos':['gte','lte'],
+            'bienes_actuales':['gte','lte'],
+            'deudas': ['gte','lte'],
+
+        }
 
 class ConsultaInformeContableFilter(django_filters.FilterSet):
     class Meta:
@@ -71,11 +117,26 @@ class ConsultaFacturaFilter(django_filters.FilterSet):
 class FacturaFilter(django_filters.FilterSet):
     class Meta:
         model = Factura
-        fields = '__all__'
+        fields = { 
+            'id':['exact'],
+            'fecha': ['gte','lte'],
+            'total': ['gte','lte'],
+            'proveedor':['icontains'],
+        }
 
 class EmpleadoTrabajaFilter(django_filters.FilterSet):
     class Meta:
         model = EmpleadoTrabaja
-        fields = '__all__'
+        fields = { 
+            'idEmpleado':['exact'],
+            'fecha_pago': ['gte','lte'],
+            'fecha_alta': ['gte','lte'],
+            'fecha_baja': ['gte','lte'],
+            'cuenta_bancaria': ['exact'],
+            'nombre':['icontains'],
+            'apellidos':['icontains'],
+            'nombre_puesto':['exact'],
+
+        }
 
 
